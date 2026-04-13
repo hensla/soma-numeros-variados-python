2fdce66220580d781bfcc5384582d381c4390b58
Henry F Ramos <137116833+hensla@users.noreply.github.com>
2026-04-12 21:04:29 -0300

    Add files via upload

---
+++ soma-numeros-variados-python/main.py
@@ -0,0 +1,8 @@
+numero = 1
+total = 0
+
+while numero != 0:
+    numero = int(input("Digite números váriados, e para encerrar e somar, digite 0"))
+    total += numero
+
+print(f"A soma de todos os valores é {total}.")
